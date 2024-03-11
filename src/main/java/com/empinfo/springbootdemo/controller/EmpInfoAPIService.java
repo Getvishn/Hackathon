01df0545810df74ec112290befcc99013b23ba54
srvk <srvktech@gmail.com>
2024-03-11 14:47:43 -0500

    springboot-demo commit

--- src/main/java/com/empinfo/springbootdemo/controller/EmpInfoAPIService.java
+++ src/main/java/com/empinfo/springbootdemo/controller/EmpInfoAPIService.java
@@ -1,7 +1,9 @@
 package com.empinfo.springbootdemo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,18 @@ public class EmpInfoAPIService {
 		return "Employee Details created sucessfully";
 	}
 	
+	@PutMapping
+	public String updateEmpDetails(@RequestBody EmpInfo empInfo)
+	{
+		this.empInfo = empInfo;
+		return "Employee Details updated sucessfully";
+	}
+	@DeleteMapping("{empId}")
+	public String deleteEmpDetails(String empId)
+	{
+		this.empInfo = null;
+		return "Employee Details deleted sucessfully";
+	}
 	
 
 }
